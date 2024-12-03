e9a741aa7bc962cd3415b60fd0dfb49d2b21bc15
SamBobBarnes <samuelrbarnes@outlook.com>
2024-12-02 23:23:16 -0600

    day 3 part 2. That was easy

--- Day3/Part2.cpp
+++ Day3/Part2.cpp
@@ -1,7 +1,42 @@
+#include <regex>
+
 #include "Day3.h"
 
 int Day3::Part2() {
-    const auto lines = Helpers::readFile(3, true);
+    const auto lines = Helpers::readFile(3);
 
-    return 0;
+    const regex find{R"(mul\([0-9]+,[0-9]+\)|do\(\)|don't\(\))"};
+
+    vector<string> matches{};
+
+    for (const auto &line: lines) {
+        string log = line;
+        for (std::smatch sm; regex_search(log, sm, find);) {
+            matches.push_back(sm.str());
+            log = sm.suffix();
+        }
+    }
+
+    int total{0};
+
+    bool disabled{false};
+
+    for (const auto &match: matches) {
+        if (match == "do()") {
+            disabled = false;
+            continue;
+        }
+        if (match == "don't()") {
+            disabled = true;
+            continue;
+        }
+        if (disabled) continue;
+        auto parts = Helpers::split(match, ',');
+        int first = stoi(parts[0].substr(4));
+        int second = stoi(parts[1].substr(0, parts[1].length() - 1));
+
+        total += first * second;
+    }
+
+    return total;
 }
