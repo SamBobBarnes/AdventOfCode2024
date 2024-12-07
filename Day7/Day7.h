7d0020623bd28d7e7d1e0bb8e766bd3e75c9ae4d
SamBobBarnes <samuelrbarnes@outlook.com>
2024-12-06 23:50:59 -0600

    dy 7 part 1

--- Day7/Day7.h
+++ Day7/Day7.h
@@ -7,19 +7,59 @@
 
 using namespace std;
 
-class Day7 {
-public:
-    static int Part1();
-
-    static int Part2();
-};
-
 struct Equation {
-    Equation(const int answer, vector<int> parts): Answer(answer), Parts(std::move(parts)) {
+    Equation(const long answer, vector<long> parts): Answer(answer), Parts(std::move(parts)) {
     }
 
-    int Answer;
-    vector<int> Parts;
+    long Answer;
+    vector<long> Parts;
+    vector<char> Operators{};
+
+    [[nodiscard]] bool Full() const {
+        return Operators.size() == Parts.size() - 1;
+    }
+
+    bool CorrectOperators() {
+        long total{Parts[0]};
+        for (int i = 0; i < Operators.size(); ++i) {
+            total = perform(total, Parts[i + 1], Operators[i]);
+        }
+        return total == Answer;
+    }
+
+private :
+    long perform(long a, long b, char _operator) {
+        switch (_operator) {
+            case '+':
+                return a + b;
+            case '*':
+                return a * b;
+        }
+    }
 };
+
+class Day7 {
+public:
+    static long Part1();
+
+    static int Part2();
+
+    static bool SolveEquation(Equation e, char operators[]);
+};
+
+inline bool Day7::SolveEquation(Equation e, char operators[]) {
+    if (e.Full()) {
+        return e.CorrectOperators();
+    }
+
+    Equation e1 = e;
+    e1.Operators.push_back(operators[0]);
+    bool e1a = SolveEquation(e1, operators);
+    Equation e2 = e;
+    e2.Operators.push_back(operators[1]);
+    bool e2a = SolveEquation(e2, operators);
+
+    return e1a || e2a;
+}
 
 #endif //DAY7_H
--- Day7/Part1.cpp
+++ Day7/Part1.cpp
@@ -1,7 +1,24 @@
 #include "Day7.h"
 
-int Day7::Part1() {
-    const auto lines = Helpers::readFile(7, true);
+long Day7::Part1() {
+    const auto lines = Helpers::readFile(7, false);
 
-    return 0;
+    vector<Equation> equations{};
+    char operators[]{'+', '*'};
+
+    for (const auto &line: lines) {
+        vector<long> parts{};
+        auto partsStrings = Helpers::split(Helpers::split(line, ':')[1].substr(1), ' ');
+        parts.reserve(partsStrings.size());
+        for (const auto &i: partsStrings) parts.push_back(stol(i));
+        equations.emplace_back(stol(Helpers::split(line, ':')[0]), parts);
+    }
+
+    long total{0};
+
+    for (const auto &e: equations) {
+        if (SolveEquation(e, operators)) total += e.Answer;
+    }
+
+    return total;
 }
