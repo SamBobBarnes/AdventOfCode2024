42018f1581e3400ba95cdee50912f1fa9ae09135
SamBobBarnes <samuelrbarnes@outlook.com>
2024-12-06 23:21:14 -0600

    equation struct

--- Day7/Day7.h
+++ Day7/Day7.h
@@ -1,6 +1,8 @@
 #ifndef DAY7_H
 #define DAY7_H
 
+#include <utility>
+
 #include "../Helpers.h"
 
 using namespace std;
@@ -10,6 +12,14 @@ public:
     static int Part1();
 
     static int Part2();
+};
+
+struct Equation {
+    Equation(const int answer, vector<int> parts): Answer(answer), Parts(std::move(parts)) {
+    }
+
+    int Answer;
+    vector<int> Parts;
 };
 
 #endif //DAY7_H
