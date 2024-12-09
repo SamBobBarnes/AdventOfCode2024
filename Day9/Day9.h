914e2fc5a82a859d1948052673168ae45c3d516a
SamBobBarnes <samuelrbarnes@outlook.com>
2024-12-08 23:01:59 -0600

    Day9 init

---
+++ Day9/Day9.h
@@ -0,0 +1,15 @@
+#ifndef DAY9_H
+#define DAY9_H
+
+#include "../Helpers.h"
+
+using namespace std;
+
+class Day9 {
+public:
+    static int Part1();
+
+    static int Part2();
+};
+
+#endif //DAY9_H
---
+++ Day9/Part2.cpp
@@ -0,0 +1,7 @@
+#include "Day9.h"
+
+int Day9::Part2() {
+    const auto lines = Helpers::readFile(9, true);
+
+    return 0;
+}
