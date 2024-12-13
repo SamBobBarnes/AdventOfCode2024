4478858c769ee0495afaa5dc5abbf36f475eed1d
SamBobBarnes <samuelrbarnes@outlook.com>
2024-12-12 23:05:06 -0600

    Day13 init

---
+++ Day13/Day13.h
@@ -0,0 +1,15 @@
+#ifndef DAY13_H
+#define DAY13_H
+
+#include "../Helpers.h"
+
+using namespace std;
+
+class Day13 {
+public:
+    static int Part1();
+
+    static int Part2();
+};
+
+#endif //DAY13_H
---
+++ Day13/Part1.cpp
@@ -0,0 +1,7 @@
+#include "Day13.h"
+
+int Day13::Part1() {
+    const auto lines = Helpers::readFile(13, true);
+
+    return 0;
+}
