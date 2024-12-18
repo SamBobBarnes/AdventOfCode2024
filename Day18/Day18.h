9142b4e199c3f95fce3e7ccb701c688b34b65ee5
SamBobBarnes <samuelrbarnes@outlook.com>
2024-12-18 00:28:47 -0600

    day 18 part 2

--- Day18/Day18.h
+++ Day18/Day18.h
@@ -2,6 +2,7 @@
 #define DAY18_H
 
 #include "../Helpers.h"
+#include "../Point.h"
 
 using namespace std;
 
@@ -9,7 +10,9 @@ class Day18 {
 public:
     static int Part1();
 
-    static int Part2();
+    static string Part2();
+
+    static void resetDijkstra(const int width, const int height, map<Point, Point> *prev, map<Point, int> *dist);
 };
 
 #endif //DAY18_H
--- Day18/Part2.cpp
+++ Day18/Part2.cpp
@@ -1,7 +1,121 @@
+#include <map>
+#include <queue>
+
 #include "Day18.h"
+#include "PointNode.h"
 
-int Day18::Part2() {
-    const auto lines = Helpers::readFile(18, true);
+void Day18::resetDijkstra(const int width, const int height, map<Point, Point> *prev, map<Point, int> *dist) {
+    for (int y = 0; y <= height; ++y) {
+        for (int x = 0; x <= width; ++x) {
+            (*prev)[{x, y}] = {};
+            (*dist)[{x, y}] = Helpers::Max;
+        }
+    }
 
-    return 0;
+    (*dist)[{0, 0}] = 0;
+}
+
+string Day18::Part2() {
+    bool example = false;
+    const auto lines = Helpers::readFile(18, example);
+
+    const int width = example ? 6 : 70;
+    const int height = example ? 6 : 70;
+    int fallenBytes = example ? 12 : 1024;
+
+    map<Point, bool> bytes{};
+
+    for (int i = 0; i < fallenBytes; i++) {
+        auto coords = Helpers::split(lines[i], ',');
+        Point p{stoi(coords[0]), stoi(coords[1])};
+        bytes[p] = true;
+    }
+
+    // Print(&bytes);
+
+    map<Point, Point> prev{};
+    map<Point, int> dist{};
+
+    Point start{0, 0};
+    const Point end{width, height};
+
+    priority_queue<PointNode> q{};
+
+    resetDijkstra(width, height, &prev, &dist);
+    q.emplace(start, 0);
+
+    while (!q.empty()) {
+        auto u = q.top();
+        if (u == end) {
+            break;
+        }
+        q.pop();
+
+        Point neighbors[4]{
+            {u.x, u.y - 1},
+            {u.x + 1, u.y},
+            {u.x, u.y + 1},
+            {u.x - 1, u.y}
+        };
+
+        for (auto v: neighbors) {
+            if (v.x < 0 || v.y < 0 || v.x > width || v.y > height || bytes[v])
+                continue;
+
+            auto alt = dist[u] + 1;
+            if (alt < dist[v]) {
+                prev[v] = u;
+                dist[v] = alt;
+                q.emplace(v.x, v.y, alt);
+            }
+        }
+    }
+
+    int topScore = q.top().Score;
+
+    int currentScore = topScore;
+
+    for (int i = fallenBytes; i < lines.size(); i++) {
+        auto coords = Helpers::split(lines[i], ',');
+        Point p{stoi(coords[0]), stoi(coords[1])};
+        bytes[p] = true;
+
+
+        resetDijkstra(width, height, &prev, &dist);
+        q = {};
+        q.emplace(start, 0);
+
+        while (!q.empty()) {
+            auto u = q.top();
+            if (u == end) {
+                break;
+            }
+            q.pop();
+
+            Point neighbors[4]{
+                {u.x, u.y - 1},
+                {u.x + 1, u.y},
+                {u.x, u.y + 1},
+                {u.x - 1, u.y}
+            };
+
+            for (auto v: neighbors) {
+                if (v.x < 0 || v.y < 0 || v.x > width || v.y > height || bytes[v])
+                    continue;
+
+                auto alt = dist[u] + 1;
+                if (alt < dist[v]) {
+                    prev[v] = u;
+                    dist[v] = alt;
+                    q.emplace(v.x, v.y, alt);
+                }
+            }
+        }
+
+        currentScore = q.top().Score;
+        if (currentScore >= topScore) topScore = currentScore;
+        else return lines[i];
+    }
+
+    return "-1";
 }
