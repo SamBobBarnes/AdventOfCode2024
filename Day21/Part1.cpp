b4219ebc7f6eb814efd87a977d77931d5d7f0ed7
SamBobBarnes <samuelrbarnes@outlook.com>
2024-12-21 10:25:11 -0600

    wip Day 21 part 1

--- Day21/Part1.cpp
+++ Day21/Part1.cpp
@@ -3,5 +3,53 @@
 int Day21::Part1() {
     const auto lines = Helpers::readFile(21, true);
 
+    auto getKeypadPos = [](const char key)-> Point {
+        switch (key) {
+            case 'A': return {2, 3};
+            case '0': return {1, 3};
+            case '1': return {1, 2};
+            case '2': return {2, 2};
+            case '3': return {0, 1};
+            case '4': return {1, 1};
+            case '5': return {2, 1};
+            case '6': return {0, 2};
+            case '7': return {0, 0};
+            case '8': return {1, 0};
+            case '9': return {2, 0};
+            default: throw;
+        }
+    };
+
+    auto getDirpadPos = [](const char key)-> Point {
+        switch (key) {
+            case '^': return {1, 0};
+            case 'A': return {2, 0};
+            case '<': return {0, 1};
+            case 'v': return {1, 1};
+            case '>': return {2, 1};
+            default: throw;
+        }
+    };
+
     return 0;
 }
+
+/*
+    +---+---+
+    | ^ | A |
++---+---+---+
+| < | v | > |
++---+---+---+
+
+
++---+---+---+
+| 7 | 8 | 9 |
++---+---+---+
+| 4 | 5 | 6 |
++---+---+---+
+| 1 | 2 | 3 |
++---+---+---+
+    | 0 | A |
+    +---+---+
+
+*/
