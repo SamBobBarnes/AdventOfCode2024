fce97cb247ec205eaf3da9b3e0acb8d731c71a7a
SamBobBarnes <samuelrbarnes@outlook.com>
2024-12-23 11:17:00 -0500

    wip part 2

--- Day22/Part2.cpp
+++ Day22/Part2.cpp
@@ -1,7 +1,24 @@
 #include <map>
+#include <set>
 #include <unordered_map>
 
 #include "Day22.h"
+
+size_t combineHashes(size_t hash1, size_t hash2) {
+    // A simple hash combination technique, you can use more sophisticated ones if needed
+    return hash1 ^ (hash2 + 0x9e3779b9 + (hash1 << 6) + (hash1 >> 2));
+}
+
+size_t hashInts(const std::array<int, 4> &ints) {
+    std::hash<int> hasher;
+    size_t combinedHash = 0;
+
+    for (int value: ints) {
+        combinedHash = combineHashes(combinedHash, hasher(value));
+    }
+
+    return combinedHash;
+}
 
 uint64_t Day22::Part2() {
     const auto lines = Helpers::readFile(22, true);
@@ -17,21 +34,6 @@ uint64_t Day22::Part2() {
         return number % 10;
     };
 
-    auto hash = [](const int a, const int b, const int c, const int d) {
-        return ((int8_t) a << 24) ^ ((int8_t) b << 16) ^ ((int8_t) c << 8) ^ ((int8_t) d);
-    };
-
-    auto unhash = [](const int32_t hashNum)-> vector<int> {
-        vector<int> nums{0, 0, 0, 0};
-
-        nums[3] = hashNum & 0x000000FF;
-        nums[2] = hashNum & 0x0000FF00;
-        nums[1] = hashNum & 0x00FF0000;
-        nums[0] = hashNum & 0xFF000000;
-
-        return nums;
-    };
-
     vector<uint64_t> numbers{};
 
     for (auto &i: lines) {
@@ -39,7 +41,8 @@ uint64_t Day22::Part2() {
     }
 
     uint64_t total{0};
-    map<int32_t, uint64_t> memo{};
+    map<size_t, uint64_t> memo{};
+    set<size_t> memoIds{};
 
     constexpr uint64_t rounds{2000};
     for (auto &number: numbers) {
@@ -48,31 +51,32 @@ uint64_t Day22::Part2() {
         for (int i = 0; i < rounds; ++i) {
             auto digit = transform(number);
             auto current = digit;
-            changes.push_back(previous - current);
+            changes.push_back(current - previous);
             previous = current;
             if (i < 4) continue;
 
             // memoize changes
 
-            int32_t memoId = hash(changes[i - 4], changes[i - 3], changes[i - 2], changes[i - 1]);
+            uint32_t memoId = hashInts({changes[i - 4], changes[i - 3], changes[i - 2], changes[i - 1]});
+            memoIds.insert(memoId);
             memo[memoId] += digit;
         }
         total += number;
     }
 
-    vector<int32_t> highest;
+    vector<size_t> highest;
     uint64_t highestValue = 0;
     for (auto [id,number]: memo) {
+        if (number == highestValue) {
+            highest.push_back(id);
+        }
         if (number > highestValue) {
             highestValue = number;
             highest = {id};
         }
-        if (number == highestValue) {
-            highest.push_back(id);
-        }
     }
 
-    auto monkey = memo[hash(-2, 1, -1, 3)];
+    auto monkey = memo[hashInts({-2, 1, -1, 3})];
 
     return total;
 }
